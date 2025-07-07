53c2c219d459149295a2e36efd853fa5d50bc8cd
bernardo.k <bernardo.kraczkowski@nsh.com.br>
2025-07-07 17:21:24 -0300

    refactor: update App component for new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '@assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useNavigate } from 'react-router-dom'
@@ -27,6 +27,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <button onClick={() => navigate('/custom')}>Ir para custom</button>
+        <button onClick={() => navigate('/pomodoro')}>Ir para pomodoro</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
