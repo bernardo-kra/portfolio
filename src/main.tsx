2116ec9c111d8e06504d2e1d228955ba2590204a
bernardo.k <bernardo.kraczkowski@nsh.com.br>
2025-07-07 17:18:36 -0300

    feat: add smooth theme transition animation

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
-import Container from './components/Container';
+import App from '@src/App.tsx'
+import Container from '@components/common/Container';
+import Pomodoro from '@pages/Pomodoro';
+import { ThemeProvider } from '@theme/ThemeContext';
+import TransitionThemeEffect from '@theme/TransitionThemeEffect';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes> 
-        <Route path="/portfolio" element={<App />} />
-        <Route path="/custom" element={<Container>Conteúdo customizado!</Container>} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+  <ThemeProvider>
+    <TransitionThemeEffect />
+    <StrictMode>
+      <BrowserRouter basename="/portfolio">
+        <Routes> 
+          <Route path="/" element={<App />} />
+          <Route path="/custom" element={<Container>Conteúdo customizado!</Container>} />
+          <Route path="/pomodoro" element={<Pomodoro />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  </ThemeProvider>,
 )
