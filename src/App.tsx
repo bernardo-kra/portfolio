1f99877d22a5179829cff4c09a4b5561191c1f3e
bernardo.k <bernardo.kraczkowski@nsh.com.br>
2025-07-07 10:03:54 -0300

    Adding new Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
+  const navigate = useNavigate()
 
   return (
     <>
@@ -24,6 +26,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <button onClick={() => navigate('/custom')}>Ir para custom</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
