10aec995594b09b9017567b30de75f17c9250172
bernardo.k <bernardo.kraczkowski@nsh.com.br>
2025-07-07 17:20:45 -0300

    chore: update project config and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  resolve: {
+    alias: {
+      '@src': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@pages': resolve(__dirname, 'src/Pages'),
+      '@theme': resolve(__dirname, 'src/components/theme'),
+    },
+  },
 })
